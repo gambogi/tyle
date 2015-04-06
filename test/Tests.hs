d23c6ccfd42e1321942287275e90af044b74351d
Matt Gambogi <m@gambogi.com>
2015-04-05 18:42:01 -0700

    added testing infrastructure

---
+++ test/Tests.hs
@@ -0,0 +1,10 @@
+import Test.HUnit
+import Test.QuickCheck
+import Text.Printf
+import Data.Monoid
+
+import Text.Parsec.String
+import Tyle.Parsers
+
+main :: IO ()
+main = putStrLn "I should be a test!"
